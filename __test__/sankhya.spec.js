f0b4e55a91c02e14303876e17f5b98f989980d81
ajitjha393 <ajitjha393@gmail.com>
2022-02-23 12:43:24 +0530

    Add test for maching prop names

--- __test__/sankhya.spec.js
+++ __test__/sankhya.spec.js
@@ -1,5 +1,20 @@
 const sankhya = require('../src/index')
 
+// Get Object's own enumerable keys without ancestor's, sorted
+
+function objectOwnKeys(o) {
+  return Object.keys(o).sort()
+}
+
+// Get All Object's enumerable keys including ancestor's, sorted
+
+function objectAllKeys(o) {
+  const allKeys = []
+  for (let k in o) {allKeys.push(k) }
+
+  return allKeys.sort(), allKeys
+}
+
 test('sankhya with only input object', () => {
   const transform = sankhya({
     fullName: i => `${i.firstName} ${i.lastName}`,
@@ -7,11 +22,12 @@ test('sankhya with only input object', () => {
       `${i.firstName.at(0).toUpperCase()}.${i.lastName.at(0).toUpperCase()}.`,
   })
 
-  const {fullName, initials} = transform({
+  const output = transform({
     firstName: 'Bishwajit',
     lastName: 'Jha',
   })
   const expected = {fullName: 'Bishwajit Jha', initials: 'B.J.'}
 
-  expect({fullName, initials}).toEqual(expected)
+  expect(output).toEqual(expected)
+  expect(objectAllKeys(output)).toEqual(['fullName', 'initials'])
 })
